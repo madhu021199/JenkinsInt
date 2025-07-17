8c7b656584dd52325fa2f2df086bd92f48c69352
madhumitha <rmadhumitha21199@gmail.com>
2025-07-17 18:36:16 +0530

    updated Launching google

--- src/main/java/demo/Launchinggoogle.java
+++ src/main/java/demo/Launchinggoogle.java
@@ -1,5 +1,7 @@
 package demo;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -10,11 +12,13 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class Launchinggoogle {
 	
 	@Test
-	public void demo() {
+	public void demo() throws InterruptedException {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://www.google.com/");
-		
+		driver.findElement(By.xpath("//textarea[@class='gLFyf']")).sendKeys("IMAGES",Keys.ENTER);
+		Thread.sleep(2000);
+		driver.close();
 
 }
 }
